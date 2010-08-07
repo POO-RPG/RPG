f486b71067cadbcf0e3b6e4e05231c1be74a1433
Tiago de França Queiroz <belimawr@belimawr-laptop.(none)>
2010-08-07 03:57:10 -0300

    criada interface envia ficha

---
+++ src/rpg/Envia_ficha.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rpg;
+
+/**
+ *
+ * @author belimawr
+ */
+public interface Envia_ficha
+{
+        void Envia_ficha(Ficha ficha);
+
+}

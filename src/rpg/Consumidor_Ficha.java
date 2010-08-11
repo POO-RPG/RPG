7f41d53bff06f3ebbfce22a8557ffd2739696ccb
Anderson Queiroz <anderson.f.queiroz@gmail.com>
2010-08-10 21:05:22 -0300

    Debug forever!

--- src/rpg/Consumidor_Ficha.java
+++ src/rpg/Consumidor_Ficha.java
@@ -48,7 +48,7 @@ public class Consumidor_Ficha extends Thread
                                 try
                                 {
                                         c.Enviar_ficha(ficha);
-					System.out.printf("Servidor enviou:\n Jogador %s | Personagem %s\n", ficha.getNome_jogador(), ficha.getNome_personagem());
+					System.out.printf("Consumidor ficha enviou:\n Jogador %s | Personagem %s\n", ficha.getNome_jogador(), ficha.getNome_personagem());
                                 }
                                 catch (Exception e)
                                 {
